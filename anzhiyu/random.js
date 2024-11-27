245a7446ec07c13c56d690a22427731231e7df28
L1ulee <dr.liulee@gmail.com>
2024-11-27 15:57:35 +0800

    Site updated: 2024-11-27 15:57:35

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/08/18/资源分享/IDA9.0 破解/","2024/10/13/资源分享/红队命令速查-洞查文库/"];function toRandomPost(){
+var posts=["2024/08/18/资源分享/IDA9.0 破解/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
