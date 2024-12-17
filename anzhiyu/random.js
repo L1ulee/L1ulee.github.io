b228ab1fae7bc702a50086696869c6132315d76a
L1ulee <dr.liulee@gmail.com>
2024-12-17 18:17:10 +0800

    Site updated: 2024-12-17 18:17:10

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/08/18/资源分享/IDA9.0 破解/","2024/10/13/资源分享/红队命令速查-洞查文库/","2024/11/27/技术分享/Hack The Box/Starting Point/Starting Point Tier 0 Meow实战笔记/"];function toRandomPost(){
+var posts=["2024/08/18/资源分享/IDA9.0 破解/","2024/10/13/资源分享/红队命令速查-洞查文库/","2024/11/15/技术分享/学习笔记/1.1 Linux基础命令/","2024/11/27/技术分享/Hack The Box/Starting Point/Starting Point Tier 0 Meow实战笔记/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
